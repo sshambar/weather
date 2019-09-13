15e22f63951d1e7b1049ba40314033e617b5103b
Scott Shambarger <devel@shambarger.net>
2019-09-13 15:36:35 +0100

    Support subdirectories and custom titles

--- etc/webconfig.php
+++ etc/webconfig.php
@@ -3,4 +3,11 @@ $db_pdo = 'mysql:host=localhost;unix_socket=/var/lib/mysql/mysql.sock;dbname=wee
 $db_user = 'weewx';
 $db_pass = 'password';
 $default_prefix = null;
-
+/* to add subdirectories... (title and source are optional)
+$prefix_map = array(
+    'home' => array(
+        'title' => 'Home',
+        'source' => 'weather2'
+    ),
+)
+*/
---
+++ html/index.php
@@ -0,0 +1,39 @@
+<?php
+$title = 'The';
+$config = '/etc/weather/webconfig.php';
+if(! is_readable($config)) {
+	throw new Exception("Unable to read config");
+}
+require_once($config);
+$source = $default_prefix;
+if(isset($_SERVER['REDIRECT_ORIGINAL_PATH'])) {
+    $source = preg_replace('|/([a-z]+)[^a-z]?.*|', '$1',
+                           $_SERVER['REDIRECT_ORIGINAL_PATH'], 1);
+}
+if(isset($prefix_map) && isset($prefix_map[$source])) {
+    $map = $prefix_map[$source];
+    if(isset($map['title'])) { $title = $map['title']; }
+    if(isset($map['source'])) { $source = $map['source']; }
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?php echo $title ?> Weather</title>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://code.highcharts.com/stock/highstock.js"></script>
+<script src="<?php echo $_SERVER['CONTEXT_PREFIX']; ?>/weather.js"></script>
+<script type="text/javascript">
+ $(function() {
+    weatherSetup(<?php echo "'$source'" ?>);
+  });
+</script>
+</head>
+<body>
+    <h1 align="middle" style="color:#3E576F"><?php echo $title ?> Weather</h1>
+    <div id="loading" align="middle" style="color:#3E576F; height:500px; min-width:310px">Loading...</div>
+    <div id="container" style="display:none; height: 500px; min-width: 310px"></div>
+</body>
+</html>
